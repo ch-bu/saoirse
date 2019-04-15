3b1fec5c492a784d31dafea17b9ff8aa7e0919b8
Christian Burkhart <burkhart.christian@gmail.com>
2019-04-15 21:54:21 +0200

    style: improve modules page

--- src/pages/modules.js
+++ src/pages/modules.js
@@ -4,34 +4,38 @@ import styled from 'styled-components'
 import { Link } from "gatsby"
 import { graphql } from "gatsby"
 import Helmet from 'react-helmet';
+import { Spring, config } from 'react-spring/renderprops.cjs';
+import { FaStar } from "react-icons/fa";
+
+
+const Container = styled.div`
+  min-width: 95vw;
+  min-height: 100vh;
+  background-color: #000;
+  background-color: ${props => props.theme.primaryColor};
+  filter: saturate(.6);
+  padding: 2.5vh;
+`;
 
 const Modules = styled.div`
-  /* background-color: ${props => props.theme.darkColor}; */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding-top: 50px;
-  padding-left: 5vw;
-  padding-right: 5vw;
-  margin: 0 auto;
-  text-align: center;
-  min-height: 100vh;
+  background-color: ${props => props.theme.asideBackgroundColor};
+  padding: 5vh;
+  min-height: 95vh;
 
   h1 {
     margin-top: 0;
-    color: #000;
+    color: #fff;
     border: none;
+    width: 100%;
+    filter: rotate(1deg);
+    border-bottom: 5px solid ${props => props.theme.primaryColor};
   }
 
   @media only screen and (min-width: ${props => props.theme.breakpointOne}) {
-    text-align: left;
-    padding-left: 20vw;
-    padding-right: 20vw;
-
     h1 {
+      margin-top: 5vh;
+      width: 60%;
       font-size: 3rem;
-      font-weight: 700;
-      margin-top: 2rem;
     }
   }
 `;
@@ -48,27 +52,25 @@ const ModulesFlexbox = styled.div`
   }
 `;
 
+
 const FlexElement = styled.div`
-  /* background-color: ${props => props.theme.darkColor}; */
-  /* background-color: #fff; */
-  background-color: rgb(245,245,245);
-  opacity: .9;
-  width: 90%;
+  background-color: ${props => props.theme.asideDropdownBackgroundColor};
+  width: 100%;
   text-align: center;
-  margin-bottom: 2rem;
-  transition: background-color 0.3s;
-  /* border: 1px solid ${props => props.theme.primaryColorLight}; */
-  box-shadow: 5px 10px 25px 0 rgba(46,61,73,.2);
+  margin-bottom: 1rem;
+  transition: all 0.3s;
+  box-sizing: border-box;
+  border-left: 10px solid ${props => props.theme.asideDropdownBackgroundColor};
   
   @media only screen and (min-width: ${props => props.theme.breakpointOne}) {
-    width: 100%;
-    margin-right: 2rem;
+    width: 33%;
+    margin-right: 5px;
     height: 180px;
   }
 
   &:hover {
-    /* background-color: ${props => props.theme.darkColorLight}; */
-    box-shadow: 2px 4px 8px 0 rgba(46,61,73,.2);
+    background-color: #4e4e4e;
+    border-left: 10px solid ${props => props.theme.primaryColor};
   }
 
   a {
@@ -82,10 +84,10 @@ const FlexElement = styled.div`
 const ModuleNumber = styled.div`
   align-self: center;
   font-size: 3rem;
-  /* color: ${props => props.theme.primaryColor}; */
-  color: #000;
+  color: ${props => props.theme.asideDropdownTitleTextColor};
   text-align: center;
   width: 29%;
+  border-right: 1px solid #626262;
 
   @media only screen and (min-width: ${props => props.theme.breakpointOne}) {
     font-size: 5rem;
@@ -94,8 +96,7 @@ const ModuleNumber = styled.div`
 
 const ModuleDescription = styled.div`
   width: 80%;
-  color: #fff;
-  color: #000;
+  color: ${props => props.theme.asideDropdownTitleTextColor};
   text-align: left;
   padding: 20px;
   align-self: center;
@@ -103,6 +104,38 @@ const ModuleDescription = styled.div`
 
   @media only screen and (min-width: ${props => props.theme.breakpointOne}) {
     font-size: 2rem;
+  }
+`;
+
+const Credits = styled.div`
+  position: fixed;
+  bottom: 5vh;
+  right: 10vh;
+  color: #fff;
+  padding: 10px;
+  padding-left: 20px;
+  background-color: ${props => props.theme.asideDropdownBackgroundColor};
+  border-radius: 5px;
+  transform: rotate(-3deg);
+  border: 1px solid #626262;
+
+  a {
+    color: ${props => props.theme.primaryColor};
+    color: #fff;
+    border-bottom: 2px solid ${props => props.theme.primaryColor};
+    text-decoration: none;
+
+    &:hover {
+      border-bottom: 2px solid ${props => props.theme.primaryColorLight};
+    }
+  }
+
+  svg {
+    color: ${props => props.theme.primaryColor};
+    position: absolute;
+    font-size: 2.5rem;
+    top: -27%;
+    left: -8%;
   }
 `;
 
@@ -114,23 +147,37 @@ export default ({ data }) => {
         <Helmet>
           <title>Your Lessons</title>
         </Helmet>
-        <Modules>
-          <h1>Lessons</h1>
-          <ModulesFlexbox>
-            {modules.map(({ node }) => (
-              
-              <FlexElement key={node.frontmatter.module}>
-                <Link to={`/module?id=` + node.frontmatter.module + 
-                            '&unit=' + node.frontmatter.unit +
-                            '&subunit=' + node.frontmatter.subunit}>
-                    <ModuleNumber>{node.frontmatter.module}</ModuleNumber>
-                    <ModuleDescription>{node.frontmatter.moduleTitle}</ModuleDescription>
-                </Link>
-              </FlexElement>
-              
-            ))}  
-          </ModulesFlexbox>
-        </Modules>
+        <Container>
+          <Modules>
+            <h1>Saoirse - An E-Learning Framework</h1>
+            <Spring
+              from={{ opacity: 0 }}
+              to={{ opacity: 1 }}
+              config={{duration: 200}}>
+              {props => 
+                <ModulesFlexbox style={props}>
+                {modules.map(({ node }) => (
+                  
+                  <FlexElement key={node.frontmatter.module}>
+                    <Link to={`/module?id=` + node.frontmatter.module + 
+                                '&unit=' + node.frontmatter.unit +
+                                '&subunit=' + node.frontmatter.subunit}>
+                        <ModuleNumber>{node.frontmatter.module}</ModuleNumber>
+                        <ModuleDescription>{node.frontmatter.moduleTitle}</ModuleDescription>
+                    </Link>
+                  </FlexElement>
+                  
+                ))}  
+              </ModulesFlexbox>
+                
+              }
+            </Spring>
+
+          <Credits><FaStar /><span>Designed and developed by<br />
+            <a target="_blank" href="https:/christianburkhart.de">Christian Burkhart</a>
+              </span></Credits>
+         </Modules>
+        </Container>
       </Shell> 
   );
 };
